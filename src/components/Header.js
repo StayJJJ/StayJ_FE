c426b675ed761fcd0f9fa1be3360df44c0044e80
keepggoing <jessica3983@naver.com>
2025-08-26 17:19:49 +0900

    overall fixing

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <header className="main-header">
         <div className="header-container">
             <div className="left-space" /> {/* 왼쪽 공간용 빈 div */}
-            <h1 className="center-text">STAYJ</h1>
+            <img src="images/logo.png" alt="STAYJ 로고" className="logo" />
             <Link to="/guest" className="mypage-button">마이페이지</Link>
         </div>
     </header>
