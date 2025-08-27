95b860bf56236bac0a2c5c9589d980f8b4cd3b53
DevJihun <kjh31021@g.skku.edu>
2025-08-27 14:05:16 +0900

    feat: Header에 로그아웃 버튼 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,22 @@
 import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './Header.css';
+import Cookies from 'js-cookie';
 
 const Header = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const isHome = location.pathname === "/";
+  const isHome = location.pathname === '/';
 
   const handleBackClick = () => {
     navigate(-1); // 이전 페이지로 이동
+  };
+
+  const logout = () => {
+    const keys = ['user_id', 'username', 'login_id', 'role', 'phone_number'];
+    keys.forEach((k) => Cookies.remove(k, { path: '/' }));
+    navigate('/login');
   };
 
   return (
@@ -28,10 +35,15 @@ const Header = () => {
         {/* 중앙 텍스트 */}
         <h1 className="center-text">STAYJ</h1>
 
-        {/* 오른쪽 마이페이지 */}
-        <Link to="/guest" className="mypage-button">
-          마이페이지
-        </Link>
+        {/* 오른쪽 영역: 로그아웃 + 마이페이지 */}
+        <div className="right-space">
+          <button className="logout-btn" onClick={logout}>
+            로그아웃
+          </button>
+          <Link to="/guest" className="mypage-button">
+            마이페이지
+          </Link>
+        </div>
       </div>
     </header>
   );
